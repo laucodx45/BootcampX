668c9e82762fa825b537e965064e9285883a50b9
Cody <codylau6233@hotmail.com>
2023-12-23 13:33:18 -0800

    Add query to get the total assitance request by student

--- 4_queries/2_Total_Student_Assistance_Requests.sql
+++ 4_queries/2_Total_Student_Assistance_Requests.sql
@@ -1,4 +1,3 @@
--- Get the total number of assistance_requests for a student
 SELECT COUNT(assistance_requests.*) as total_assistances, students.name
 FROM assistance_requests
 JOIN students ON students.id = student_id
