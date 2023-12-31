b902fb6aef88b1e410c1e7777528582122286e67
Cody <codylau6233@hotmail.com>
2023-12-30 21:15:39 -0800

    feat: -Parameterized queries to mitigate injection vulnerabilities.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,17 +6,21 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const limit = process.argv[3] || 5;
+const cohortName = `%${process.argv[2]}%`;
+const values = [cohortName, limit];
 
-
-pool.query(`
+const queryString = `
 SELECT students.id, students.name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON students.cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || [5]};
-`)
-.then(res => {
-    res.rows.forEach(user => {
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+pool.query(queryString, values)
+  .then((res) => {
+    res.rows.forEach((user) => {
       console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
-    })
-});
+    });
+  });
